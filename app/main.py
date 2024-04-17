62e794e43d9a15b2d63c99341b13b61377e72d38
Gagan! <preetgagansingh481@gmail.com>
2024-04-18 00:22:06 +0530

    djbvd

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # Uncomment this to pass the first stage
 import socket
 import threading
+CRLF= "\r\n"
 def receive_client(conn):
     pong = "+PONG\r\n"
     while conn.recv(1024):
@@ -21,7 +22,23 @@ def main():
         t2 = threading.Thread(target=receive_client, args=(conn,))
         t1.start()
         t2.start()
-
+def handle_connection(client_conn, client_addr):
+    with client_conn:
+        while True:
+            data = client_conn.recv(1024)
+            if not data:
+                break
+            response = handle_commands(data)
+            client_conn.sendall(response)
+def handle_commands(received_data: bytes) -> bytes:
+    decoded_request = received_data.decode("utf-8").split(CRLF)
+    cleaned_data = [decoded_request[i] for i in range(2, len(decoded_request), 2)]
+    print(cleaned_data)
+    match cleaned_data[0].upper():
+        case "PING":
+            return b"+PONG\r\n"
+        case "ECHO":
+            return f"+{cleaned_data[1]}\r\n".encode()
 
 if __name__ == "__main__":
     main()
