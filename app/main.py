0dfb6b61b559c09df293e4229f5df587ac5e5be0
Gagan! <preetgagansingh481@gmail.com>
2024-04-17 22:54:12 +0530

    yesss

--- app/main.py
+++ app/main.py
@@ -10,10 +10,11 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, add = server_socket.accept()  # wait for client
-    with conn:
-        pong_string = "+PONG\r\n"
-
-        conn.send(pong_string.encode())
+    while True:
+        message = conn.recv(1024).decode()
+        if "ping" in message:
+            pong_string = "+PONG\r\n"
+            conn.send(pong_string.encode())
 
 
 if __name__ == "__main__":
