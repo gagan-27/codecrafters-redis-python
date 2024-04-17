e79e628e5b8aff6a0b6db164aaa215bf07bd1a4a
Gagan! <preetgagansingh481@gmail.com>
2024-04-18 00:29:18 +0530

    done

--- app/main.py
+++ app/main.py
@@ -1,30 +1,25 @@
 # Uncomment this to pass the first stage
 import socket
 import threading
-CRLF= "\r\n"
 
-
+CRLF = "\r\n"
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-     
     while True:
-        conn, addr = server_socket.accept()
-        t1 = threading.Thread(target=handle_connection, args=(conn,))
-        t2 = threading.Thread(target=handle_connection, args=(conn,))
-        t1.start()
-        t2.start()
+        client_conn, client_addr = server_socket.accept()
+        t = threading.Thread(target=handle_connection, args=(client_conn, client_addr))
+        t.start()
 def handle_connection(client_conn, client_addr):
     with client_conn:
         while True:
             data = client_conn.recv(1024)
             if not data:
                 break
+            
             response = handle_commands(data)
+
             client_conn.sendall(response)
 def handle_commands(received_data: bytes) -> bytes:
     decoded_request = received_data.decode("utf-8").split(CRLF)
@@ -34,7 +29,7 @@ def handle_commands(received_data: bytes) -> bytes:
         case "PING":
             return b"+PONG\r\n"
         case "ECHO":
-            return f"+{cleaned_data[1]}\r\n".encode()
 
+            return f"+{cleaned_data[1]}\r\n".encode()
 if __name__ == "__main__":
     main()
