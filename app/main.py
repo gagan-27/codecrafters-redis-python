99e3c5b3fb7ef24573e6d203220636022b3fd051
Gagan! <preetgagansingh481@gmail.com>
2024-04-17 22:47:00 +0530

    done

--- app/main.py
+++ app/main.py
@@ -10,6 +10,12 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
+    client_socket, client_address = server_socket.accept()
+    client_socket.recv(1024)
+    res = "+PONG\r\n"
+
+    client_socket.send(res.encode())
+    client_socket.close()
 
 
 if __name__ == "__main__":
