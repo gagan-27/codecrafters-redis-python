db7fae5f899859409528b8380438a9641e565470
Gagan! <preetgagansingh481@gmail.com>
2024-04-30 00:47:44 +0530

    lkdndv

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import base64
 import random
-
 import socket
 import string
 import threading
@@ -22,104 +21,6 @@ def handle_new_client(server_socket, state):
                 t = threading.Thread(target=slave_handler.handle_msg, args=(cs, state))t.start()
 
             state.threads.append(t)
-"""def handle_msg(sock, state, for_replica=False):
-    with state.lock:
-        handler = state.sock_handler_map[sock]
-    while True:
-        cmds = handler.next_msg()
-        msg_len = handler.last_msg_len()
-        # hacky, could be binary
-        cmds = [cmd.decode() for cmd in cmds]
-        print("next msg:", cmds, "len: ", msg_len)
-        match cmds[0].lower():
-            case "ping":
-                if not for_replica:
-                    sock.sendall("+PONG\r\n".encode())
-            case "replconf":
-                if state.role == Role.SLAVE:
-                    if cmds[1].lower() == "getack":
-                        sock.sendall(
-                            encode_array(
-                                ["REPLCONF", "ACK", str(state.slave_offset)]
-                            ).encode()
-                        )
-                else:
-                    sock.sendall("+OK\r\n".encode())
-            case "psync":
-                if state.role == Role.MASTER:
-                    sock.sendall(f"+FULLRESYNC {state.replid} 0\r\n".encode())
-                    rdb_msg = f"${len(state.rdb_content)}\r\n"
-                    sock.sendall(rdb_msg.encode() + state.rdb_content)
-                    # Start to track replica
-                    with state.lock:
-                        state.repl_socks.append(sock)
-            case "wait":
-                if state.role == Role.MASTER:
-                    with state.lock:
-
-                        sock.sendall(f":{len(state.repl_socks)}\r\n".encode())
-            case "set":
-                k = cmds[1]
-                v = cmds[2]
-                if len(cmds) > 3:
-                    assert cmds[3].lower() == "px", "Only px option supported now."
-                    expire_ts = int(cmds[4]) + ts_ms()
-                else:
-                    expire_ts = None
-                with state.lock:
-                    state.kv[k] = Value(v=v, ts=expire_ts)
-                if state.role == Role.MASTER:
-                    with state.lock:
-                        if len(state.repl_socks) > 0:
-                            for slave in state.repl_socks:
-                                slave.sendall(encode_array(cmds).encode())
-                    sock.sendall("+OK\r\n".encode())
-            case "get":
-                k = cmds[1]
-                with state.lock:
-                    v = state.kv.get(k, None)
-                get_return = null_bulk_string()
-                if v is not None:
-                    if v.ts is None or ts_ms() < v.ts:
-                        get_return = bulk_string(v.v)
-                sock.sendall(get_return.encode())
-            case "info":
-                if not for_replica:
-                    assert cmds[1].lower() == "replication"
-                    d = {"role": state.role.value}
-                    if state.role == Role.MASTER:
-                        d["master_replid"] = state.replid
-                        d["master_repl_offset"] = 0
-                    res = "\n".join([f"{k}:{v}" for k, v in d.items()])
-                    sock.sendall(bulk_string(res).encode())
-            case "echo":
-                if state.role == Role.MASTER:
-                    v = cmds[1]
-                    sock.sendall(bulk_string(v).encode())
-            case cmd:
-                raise RuntimeError(f"{cmd} is not supported yet.")
-
-        state.slave_offset += msg_len
-# def handle_clients(read_sockets, all_sockets, server_socket):
-#     for sock in read_sockets:
-#         if sock == server_socket:
-#             cs, _ = server_socket.accept()
-#             all_sockets.append(cs)
-#             _sock_handler_map[cs] = RespHandler(cs)
-#             print("Accepted a new connection.")
-#         else:
-#             try:
-#                 data = sock.recv(1024)
-#                 if not data:
-#                     print("Client disconnect")
-#                     all_sockets.remove(sock)
-#                     sock.close()
-#                 else:
-#                     handle_req(sock, data.decode())
-#             except socket.error as e:
-#                 print(f"Socket error: {e}")
-#                 all_sockets.remove(sock)
-#                 sock.close()"""
 def main():
     server_socket = socket.create_server(
         ("localhost", _state.port), backlog=5, reuse_port=True
@@ -196,6 +97,6 @@ if __name__ == "__main__":
     if _state.role == Role.MASTER:
         replid = get_replid()
         _state.replid = replid
-    main() #ggwp
+    main() 
 
     _state.threads[0].join()
