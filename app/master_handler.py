84d780cf2f8942bcc771a9a945d8266706220f80
Gagan! <preetgagansingh481@gmail.com>
2024-04-30 11:22:19 +0530

    dvjb

--- app/master_handler.py
+++ app/master_handler.py
@@ -1,6 +1,7 @@
 import socket
 import bisect
 import time
+from typing import Tuple
 from app.common import (
     State,
     Value,
@@ -258,43 +259,46 @@ def handle_msg(sock: socket.socket, state: State):
                     sock.sendall(to_return.encode())
             case "xread":
                 assert cmds[1].lower() == "streams"
-                stream_key = cmds[2]
-                start_entry = cmds[3]
+                stream_key_start_pairs = extract_key_start_pairs(cmds[2:])
+                print(stream_key_start_pairs, " halu")
                 res = []
                 with state.lock:
-                    if stream_key not in state.skv:
-                        sock.sendall(encode_array([]).encode())
-                    arr = sorted(
-                        state.skv[stream_key].keys(), key=stream_entry_key_func
-                    )
-                    l = bisect.bisect_right(
-                        arr,
-                        stream_entry_key_func(start_entry),
-                        key=stream_entry_key_func,
-                    )
-                    if l == len(arr):
-                        sock.sendall(encode_array(res).encode())
-                    r = max(len(arr) - 1, 0)
-                    for i in range(l, r + 1):
-                        entry = arr[i]
-                        kvs = state.skv[stream_key][entry]
-                        flat = []
-                        for k, v in kvs.items():
-                            flat.append(k)
-                            flat.append(v)
-                        encode_flat = encode_array(flat)
-                        res.append(
-                            encode_array(
-                                [bulk_string(entry), encode_flat], bulk_encode=False
-                            )
+                    for stream_key, start_entry in stream_key_start_pairs:
+                        one_return = fetch_all_given_stream_key(
+                            state, stream_key, start_entry
                         )
-                    to_return = encode_array(res, bulk_encode=False)
-                    # needs to add one extra wrap
-                    final_return = encode_array(
-                        [bulk_string(stream_key), to_return], bulk_encode=False
-                    )
-                    final_return = encode_array([final_return], bulk_encode=False)
-                    print("XREAD return: ", final_return)
-                    sock.sendall(final_return.encode())
+                        res.append(one_return)
+                    final_returns = encode_array(res, bulk_encode=False)
+                    print("XREAD return: ", final_returns)
+                    sock.sendall(final_returns.encode())
             case cmd:
                 raise RuntimeError(f"{cmd} is not supported yet on master.")
+def fetch_all_given_stream_key(state, stream_key, start_entry):
+    res_entry_list = []
+    arr = sorted(state.skv[stream_key].keys(), key=stream_entry_key_func)
+    l = bisect.bisect_right(
+        arr,
+        stream_entry_key_func(start_entry),
+        key=stream_entry_key_func,
+    )
+    if l == len(arr):
+        res_encoded = encode_array(res_entry_list).encode()
+    else:
+        r = max(len(arr) - 1, 0)
+        for i in range(l, r + 1):
+            entry = arr[i]
+            kvs = state.skv[stream_key][entry]
+            flat = []
+            for k, v in kvs.items():
+                flat.append(k)
+                flat.append(v)
+            encode_flat = encode_array(flat)
+            res_entry_list.append(
+                encode_array([bulk_string(entry), encode_flat], bulk_encode=False)
+            )
+        res_encoded = encode_array(res_entry_list, bulk_encode=False)
+    # needs to add one extra wrap
+    key_res_encoded = encode_array(
+        [bulk_string(stream_key), res_encoded], bulk_encode=False
+    )
+    return key_res_encoded
